8b8221deaf6f515b54dbe7a1e54b337ef8de2458
talkwaybr <39776573+talkwaybr@users.noreply.github.com>
2023-07-04 14:39:20 -0300

    first

---
+++ frontend/build/precache-manifest.a786ea8ba794f7b2ac3929f3c190cb8e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b5ceb89ef84e7b06248035ed3ce1d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4e0bb4e71e72215d2df",
+    "url": "/static/css/2.fa426b8c.chunk.css"
+  },
+  {
+    "revision": "b4e0bb4e71e72215d2df",
+    "url": "/static/js/2.54535c0b.chunk.js"
+  },
+  {
+    "revision": "e32ccaf4a0109157023fabb2ad785ad7",
+    "url": "/static/js/2.54535c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "196215fb735e66ea6605",
+    "url": "/static/js/3.a99d215a.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.a99d215a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e52e4110be727f9eca6",
+    "url": "/static/js/main.7be0fd1b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.7be0fd1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf13a2520eb78d7a6062",
+    "url": "/static/js/runtime-main.bcd1eaa5.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "92b2fd98e66c2c3e76136200d2dae7eb",
+    "url": "/static/media/logo.92b2fd98.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "120c37cd71174d80f2163eeac44a2d3e",
+    "url": "/static/media/sound.120c37cd.mp3"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  }
+]);
